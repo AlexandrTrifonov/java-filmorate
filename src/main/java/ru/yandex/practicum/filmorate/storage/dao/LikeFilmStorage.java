cdc918a19aa0c5c1822846d98c8e8c0a4227f64d
Alexandr Trifonov <ast89646421790@yandex.ru>
2023-04-16 23:06:28 +0300

    Тз 11 fix: checkstyle ver5

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/LikeFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/LikeFilmStorage.java
@@ -3,5 +3,6 @@ package ru.yandex.practicum.filmorate.storage.dao;
 public interface LikeFilmStorage {
 
     void addLikeFilm(Integer filmId, Integer userId);
+
     void deleteLikeFilm(Integer filmId, Integer userId);
 }
