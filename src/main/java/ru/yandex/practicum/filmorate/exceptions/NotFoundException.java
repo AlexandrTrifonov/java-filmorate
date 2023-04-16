c2d248c82ba4825c3d0935faf0a91606db65b293
Alexandr Trifonov <ast89646421790@yandex.ru>
2023-04-16 22:53:17 +0300

    Тз 11 fix: checkstyle ver2

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/NotFoundException.java
@@ -1,12 +1,13 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
 public class NotFoundException extends RuntimeException{
+
     public static void throwException(String format, Integer id) {
         String message = format + " c id=" + id;
         throw new NotFoundException(message);
     }
 
-    public NotFoundException(String message) {
+    public NotFoundException (String message) {
         super(message);
     }
 }
